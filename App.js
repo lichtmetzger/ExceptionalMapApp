0cae1b40e591ea5a4bdce53b272e5f85045773a3
Danny Schmarsel <dsc@qbus.de>
2023-01-28 19:52:34 +0100

    Add LeafletView React component
    This fails in preview mode, but works on a real device.

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LatLng, LeafletView } from 'react-native-leaflet-view';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LeafletView
+        // The rest of your props, see the list below
+      />
       <StatusBar style="auto" />
     </View>
   );
