b48c7a9f5e37b56d58f685b74e3e0fe3a9a2664b
Danny Schmarsel <dsc@qbus.de>
2023-02-19 16:26:48 +0100

    Supply local copy of POI data
    SVG URLs aren't rendered properly, so a default marker icon is used for the moment.

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { LatLng, LeafletView } from 'react-native-leaflet-view';
+import Poi from './assets/poi.json';
 
 const DEFAULT_COORDINATE = {
-  lat: 53.59171,
-  lng: 12.08928,
+  lat: "53.59171",
+  lng: "12.08928",
 };
+
+const parsedMarkers = Poi.map((item, i) => (
+  {
+    ["position"]: item.details.coordinates,
+    //["icon"]: item.details.icon,
+    ["icon"]: '📍',
+    ["size"]: [32, 32],
+    ["title"]: item.title.rendered,
+    ["id"]: i,
+    ["excerpt"]: item.details.excerpt,
+  }
+));
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LeafletView
-        mapMarkers={[
-          {
-            position: DEFAULT_COORDINATE,
-            icon: '📍',
-            size: [32, 32],
-            title: 'Goldberg Zentrum',
-            id: 1
-          }
-        ]}
+        mapMarkers={parsedMarkers}
         mapCenterPosition={DEFAULT_COORDINATE}
       />
       <StatusBar style="auto" />
