4cf0e2644a926513f22ecf0602cdb1ce23c5fad2
Danny Schmarsel <dsc@qbus.de>
2023-02-19 14:46:22 +0100

    Center map on Goldberg

--- App.js
+++ App.js
@@ -2,11 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { LatLng, LeafletView } from 'react-native-leaflet-view';
 
+const DEFAULT_COORDINATE = {
+  lat: 53.59171,
+  lng: 12.08928,
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
       <LeafletView
-        // The rest of your props, see the list below
+        mapMarkers={[
+          {
+            position: DEFAULT_COORDINATE,
+            icon: '📍',
+            size: [32, 32],
+          },
+        ]}
+        mapCenterPosition={DEFAULT_COORDINATE}
       />
       <StatusBar style="auto" />
     </View>
