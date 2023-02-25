77e0c1a4777683db6b653eb247fe3a328cc7e417
Danny Schmarsel <dsc@qbus.de>
2023-02-25 17:22:07 +0100

    [WIP] Add fake modal when pressing the QRCode button
    A real modal would disable all of the buttons, so we need to create a component with absolute positioning.
    Can't figure out how to give this a top-margin while also being full height, need to investigate further.

--- components/TopMenu.js
+++ components/TopMenu.js
@@ -4,26 +4,39 @@ import Icons from '../assets/icons.json';
 import { SvgXml } from "react-native-svg";
 
 function TopMenu() {
-      return (
-        <View style={styles.container}>
-            <View style={styles.logoArea}>
-              <Text>LOGO</Text>
-            </View>
-            <View style={styles.navArea}>
-              <Pressable
-                onPress={() => {console.log("CAMERA")}} style={styles.settingsIcon}>
-                <SvgXml xml={Icons.camera} width="30px" height="30px"/>
-              </Pressable>
-              <Pressable
-                onPress={() => {console.log("SEARCH")}} style={styles.settingsIcon}>
-                <SvgXml xml={Icons.search} width="26px" height="26px"/>
-              </Pressable>
-              <Pressable
-                onPress={() => {console.log("SETTINGS")}} style={styles.settingsIcon}>
-                <SvgXml xml={Icons.settings} width="27px" height="27px"/>
-              </Pressable>
-            </View>
-        </View>);
+  const [qrModalVisible, setQrModalVisible] = useState(false);
+  const [searchModalVisible, setSearchModalVisible] = useState(false);
+  const [settingsModalVisible, setSettingsModalVisible] = useState(false);
+
+  return (
+    <>
+      <View style={styles.container}>
+          <View style={styles.logoArea}>
+            <Text>LOGO</Text>
+          </View>
+          <View style={styles.navArea}>
+            <Pressable
+              onPress={() => {setQrModalVisible(!qrModalVisible)}} style={styles.settingsIcon}>
+              <SvgXml xml={Icons.camera} width="30px" height="30px"/>
+            </Pressable>
+            <Pressable
+              onPress={() => {{setSearchModalVisible(!searchModalVisible)}}} style={styles.settingsIcon}>
+              <SvgXml xml={Icons.search} width="26px" height="26px"/>
+            </Pressable>
+            <Pressable
+              onPress={() => {{setSettingsModalVisible(!settingsModalVisible)}}} style={styles.settingsIcon}>
+              <SvgXml xml={Icons.settings} width="27px" height="27px"/>
+            </Pressable>
+          </View>
+      </View>
+      {qrModalVisible &&
+        <View style={styles.modal}>
+          <Text>I'm a pseudo modal</Text>
+        </View>
+      }
+    </>
+    
+    );
 };
 
 export default TopMenu;
@@ -51,5 +64,14 @@ const styles = StyleSheet.create({
   },
   settingsIcon: {
     padding: 7,
+  },
+  modal: {
+    backgroundColor: '#ffffff',
+    padding: 20,
+    top: 58,
+    left: 0,
+    width: '100%',
+    position: 'absolute',
+    zIndex: 9999
   }
 });
